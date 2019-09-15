315fc85692f7bb17aca6fe62a5502fc995c49ac6
svyborov <sergey-vyborov@yandex.ru>
2019-09-15 14:07:13 +0300

    add ArticleEntity

---
+++ src/article/article.entity.ts
@@ -0,0 +1,47 @@
+import { BeforeUpdate, Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { UserEntity } from '../user/user.entity';
+import { Comment } from './comment.entity';
+
+@Entity('article')
+export class ArticleEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  slug: string;
+
+  @Column()
+  title: string;
+
+  @Column({ default: '' })
+  description: string;
+
+  @Column({ default: '' })
+  body: string;
+
+  @Column()
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
+  updatedAt: Date;
+
+  @BeforeUpdate()
+  updateTimestamp() {
+    this.updatedAt = new Date();
+  }
+
+  @Column('simple-array')
+  tagList: string[];
+
+  @ManyToOne(type => UserEntity, user => user.articles)
+  author: UserEntity;
+
+  @OneToMany(type => Comment, comment => comment.article, {eager: true})
+  @JoinColumn()
+  comments: Comment[];
+
+  @Column({default: 0})
+  favoriteCount: number;
+}
